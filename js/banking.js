412c38f153391d9b6f60a7b63fc56b62dc63537b
shagors <shajjadshagor@gmail.com>
2022-02-18 17:34:05 +0600

    all work done

--- js/banking.js
+++ js/banking.js
@@ -5,17 +5,20 @@ function getInputValue(fieldId) {
     inputField.value = '';
     return value;
 }
+function getInnerTextValue(fieldId){
+    const fieldTag = document.getElementById(fieldId);
+    const fieldValueInText = fieldTag.innerText;
+    const value = parseFloat(fieldValueInText);
+    return value;
+}
 function updateTotal(fieldId, amount){
-    const totalTag = document.getElementById(fieldId);
-    const previousTotalInText = totalTag.innerText;
-    const previousTotal = parseFloat(previousTotalInText);
+    const previousTotal = getInnerTextValue(fieldId);
     const newTotal = previousTotal + amount;
-    totalTag.innerText = newTotal;
+    document.getElementById(fieldId).innerText = newTotal;
+    return newTotal;
 }
 function updateBalance(amount, isAdding){
-    const balanceTag =document.getElementById('balance-total');
-    const balanceInText = balanceTag.innerText;
-    const previousBalance = parseFloat(balanceInText);
+    const previousBalance = getInnerTextValue('balance-total');
     let newBalance;
     if(isAdding == true){
         newBalance = previousBalance + amount;
@@ -23,19 +26,30 @@ function updateBalance(amount, isAdding){
     else{
         newBalance = previousBalance - amount;
     }
-    balanceTag.innerText = newBalance;
+    document.getElementById('balance-total').innerText = newBalance;
 }
 
 document.getElementById('deposit-button').addEventListener('click', function(){
     const amount = getInputValue('deposit-input');
-    updateTotal('deposit-total', amount);
-    updateBalance(amount, true);
+    if (amount > 0) {
+        updateTotal('deposit-total', amount);
+        updateBalance(amount, true);
+    }
+    else{
+        alert('You entered negative or string value');
+    }
 });
 
 // handle withdraw
 
 document.getElementById('withdraw-button').addEventListener('click', function(){
     const amount = getInputValue('withdraw-input');
-    updateTotal('withdraw-total', amount);
-    updateBalance(amount, false);
+    const balance = getInnerTextValue('balance-total');
+    if (amount > 0 && amount <= balance) {
+        updateTotal('withdraw-total', amount);
+        updateBalance(amount, false);
+    }
+    else{
+        alert('You entered negative or string value');
+    }
 });
